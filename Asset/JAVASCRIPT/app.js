0d9735974c4a89d0f011ff62921ced965c2283a4
laurabaoye <laurabaoye@gmail.com>
2019-10-30 00:33:43 -0700

    laura

--- Asset/JAVASCRIPT/app.js
+++ Asset/JAVASCRIPT/app.js
@@ -2,3 +2,28 @@ $(document).ready(function () {
     var searchBtn = $('#submit');
 
 });
+
+// search input is null, alert
+function validate() {
+    var search = document.getElementById("textInput");
+
+    if (search.value.trim() == '') {
+        alert('what type of beer you want?');
+        return false;
+    } else {
+        true;
+    }
+}
+
+// login input is null, alert 
+function validatelog() {
+    var username = document.getElementById('userN')
+    var password = document.getElementById('pW')
+    if (username.value.trim() == '' || password.value.trim() == '') {
+
+        alert("No Blank value allowed");
+        return false;
+    } else {
+        true;
+    }
+}
