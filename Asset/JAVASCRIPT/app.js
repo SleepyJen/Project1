c4c21985dd89cc20a6a258fabf0025e443db5e73
shalemiez <shalemie@icloud.com>
2019-10-30 21:35:44 -0700

    fixing search button

--- Asset/JAVASCRIPT/app.js
+++ Asset/JAVASCRIPT/app.js
@@ -73,9 +73,9 @@ $(document).ready(function () {
             console.log(err);
         })
     });
-    $('#submitButton').on('click',function () {
-        window.location.href=('search.html');
-        
+    $('#submitButton').on('click', function () {
+        window.location.href = ('search.html');
+
     });
 });
 
